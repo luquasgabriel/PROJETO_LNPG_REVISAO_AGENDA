d185d67f08dcb71acfb86cadd7dde17cd76a8d82
Lucas Gabriel <138629420+luquasgabriel@users.noreply.github.com>
2023-12-13 20:23:32 -0300

    Update main.py

--- atv 13-12-2023/main.py
+++ atv 13-12-2023/main.py
@@ -1,22 +1,45 @@
 import tkinter as tk
 from tkinter import *
+import re
+
 
 main = Tk()
 main.title("agenda")
 
+
+main.resizable(0, 0)
+
+width = 400
+height = 300
+posx = main.winfo_screenwidth()/2 - (400/2)
+posy = main.winfo_screenheight()/2 - (300/2)
+main.geometry("%dx%d+%d+%d" %(width, height, posx, posy))
+
 okay = ""
 
 def verifica_campo():
-    if namecaixa.get() or emailcaixa.get() or telefonecaixa.get() == "":
+    if not namecaixa.get() == "" or not emailcaixa.get() == "" or not telefonecaixa.get() == "":
+        okay = ""
+        ok.config(text=okay)
+    if namecaixa.get() == "" or emailcaixa.get() == "" or telefonecaixa.get() == "":
         okay = "Todos os campos devem estar preenchidos"
         ok.config(text=okay)
+    else:
+        if not telefonecaixa.get().isdigit():
+            okay = "O número de telefone deve conter apenas digitos!"
+            ok.config(text=okay)
+        
+        if not "@" in emailcaixa.get() and "." not in emailcaixa.get():
+            okay = "O e-mail deve conter um @ e um ."
+            ok.config(text=okay)
+        
 
         
 agenda_title = Label(main, text = "Agenda de Contatos")
 agenda_title.pack()
 
 name = Label(main, 
-    text = "Nome"
+        text = "Nome"
 
 )
 name.pack()
